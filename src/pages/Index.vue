bc19f8ea840214d3eb67bdc9177d5e7eecd1d0e3
fmeyer1980 <fm@erhvervswebdesign.dk>
2019-04-26 16:30:34 +0200

    tt

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <h1>Welcome to my blog :)</h1>
+    <h1>Welcome to my blog</h1>
     <ul class="post-list">
       <li v-for="{ node } in $page.allWordPressPost.edges" :key="node.id">
         <Post :post="node" />
